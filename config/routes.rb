b7391f9c747478ccc3fff2ed86e4afb8173d4d07
Derek Pham <derekmpham@gmail.com>
2017-01-09 22:06:08 -0800

    Create home and login pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   	resources :recommendations
   end
 
+  root 'application#index'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
