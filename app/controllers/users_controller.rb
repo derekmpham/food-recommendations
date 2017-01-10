65fe4ccf2b922d45df70932912e036ba8d5bda1b
Derek Pham <derekmpham@gmail.com>
2017-01-09 22:35:06 -0800

    Create user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
   	end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @recommendations = Recommendation.all
+  end
+
   private
 
   def user_params
