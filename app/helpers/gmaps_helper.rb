7713cae634f127726ca8e7c32e19d2cf94cc3fe7
Derek Pham <derekmpham@gmail.com>
2017-01-23 18:10:31 -0800

    Take out gmaps static api since don't need it anymore

--- app/helpers/gmaps_helper.rb
+++
@@ -1,5 +0,0 @@
-module GmapsHelper
-	def google_map(center)
-		"https://maps.googleapis.com/maps/api/staticmap?center=\"#{center}\"&size=300x300&zoom=17&markers=color:red\|\"#{center}\"&key=#{ENV["GOOGLE_MAPS_GEOCODING_KEY"]}"
-	end
-end
