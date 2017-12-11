2ee9560a98ee4953d1cb2f81d0c639d986e74e37
Derek Pham <derekmpham@gmail.com>
2017-12-11 15:55:04 -0800

    Add sign in feature capybara spec

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -2,5 +2,15 @@ describe "sign-in process", :type => :feature do
 	before :each do
 		User.make(email: 'user@example.com', username: 'user', password: 'password')
 	end
-	
+
+	it "signs me in" do
+		visit '/login'
+		within("#username") do
+			fill_in 'Username', with: 'user'
+			fill_in 'Password', with: 'password'
+		end
+		click_button 'Submit'
+		expect(page).to have_content 'Hey user!'
+
+	end
 end
