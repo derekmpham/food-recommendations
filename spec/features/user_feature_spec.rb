7911db429663da56008fdc73da115dac09c04352
Derek Pham <derekmpham@gmail.com>
2017-12-11 16:46:58 -0800

    Debug sign in capybara spec

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,16 +1,17 @@
+require 'rails_helper'
+
 describe "sign-in process", :type => :feature do
 	before :each do
-		User.make(email: 'user@example.com', username: 'user', password: 'password')
+		User.create(email: 'user@example.com', username: 'user', password: 'password')
 	end
 
 	it "signs me in" do
 		visit '/login'
-		within("#username") do
-			fill_in 'Username', with: 'user'
-			fill_in 'Password', with: 'password'
+		within('form') do
+			fill_in 'username', with: 'user'
+			fill_in 'password', with: 'password'
 		end
 		click_button 'Submit'
 		expect(page).to have_content 'Hey user!'
-
 	end
 end
