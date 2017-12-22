c4a02ca907b0aafb20a3551a5361b54610a5aab3
Derek Pham <derekmpham@gmail.com>
2017-12-21 22:02:02 -0800

    Add check login page for appropriate login  path using capybara

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
 
-describe "sign-in process", :type => :feature do
+describe "User Features", :type => :feature do
+
+	# describe "new user" do
+		# click sign up
+		# enters new user email, username, password, and confirm password
+		# click_button 'Submit'
+		# expect(page).to have_content 'Hey {username}!'
+	# end
+
 	before :each do
-		User.create(email: 'user@example.com', username: 'user', password: 'password')
+		test_user = User.create(email: 'user@example.com', username: 'user', password: 'password')
 	end
 
 	it "signs me in" do
 		visit '/login'
+		expect(page).to have_current_path(login_path)
+		
 		within('form') do
 			fill_in 'username', with: 'user'
 			fill_in 'password', with: 'password'
 		end
 		click_button 'Submit'
 		expect(page).to have_content 'Hey user!'
+		# expect(page).to have_current_path(user_recommendations_path(:id))
+		# expect(page).to have_current_path(people_path(search: 'name'))
 	end
+
+	# next things to test
+	# sign in successfully (already have-->integrate rest of specs into signing in?)
+	# ready other people's food recommendations?
+	# successfully post recommendation
 end
